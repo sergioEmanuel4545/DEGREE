b13107dbf347875473897f9eeaebcaed7a3c7cf5
Sergio Emanuel <ser610.goy@gmail.com>
2020-03-04 08:17:40 -0400

    hasta alerts 4march

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -2,38 +2,43 @@
 const express = require('express');
 const router = express.Router();
 
-//crear una ruta que ke permita al usu ver un formulario
+//crear una ruta que le permita al usu ver un formulario
 router.get('/notes/add', (req,res) =>{
-    res.render('notes/new-note')
+    res.render('notes/new-note') //tenemos que crear un formulario(vista) en notes
 })
 router.get('/notes', (req,res) =>{
-    res.send('Notes from databse');
+    res.send('Notes from database');
 })
 
+//toda este procedimiento es par guardar los datos que el usuario me envia a la base de datos
 //creamos una ruta especifica para recibir datos, que envia un formulario
 //peticion tipo POST voya recibirlo a una ruta especifica , el post tiene que estar indicado como un metodo en donde envie el formulario
 router.post('/notes/new-notes', (req, res) => {
-    const {title, description}=req.body;
-    const errors = [];
-    if(!title){
-        errors.push({text: 'Please write a title'});
+    //observamos que es un objeto deJS xq tiene llaves y adentro tiene propiedades, cada uno de los datos que envia el usuario es una propiedad
+    //DESCTRUCTURING, poder sacar cada propiedad por separado en una constante o variable a partir de un objeto
+    const {title, description}= req.body;//quiero destructurar el title y el descrption, puden estar almacenados dentro una variable o constante, en este caso sera constante 
+    //PROCESO DE VALIDACION
+    const errors = [];//creamos la constante errors como un arreglo
+    if(!title){//si en lo que me envia el usuario esta vacio
+        errors.push({text: 'Please write a title'});//insertamos en la const errors, un objeto que tendra como propiedad text: que tendra como valor:..
     }
-    if(!description){
+    if(!description){//si usuario no envia description o si este se encuentra vacio
         errors.push({text: 'please write a description' });
     }
-    if(errors.length > 0){
-        res.render('notes/new-note', {
-            errors,
+    if(errors.length > 0){//si el arreglo errors contine errores
+        res.render('notes/new-note', {//renderizamos nuevamente la vista de la insercion de los formularios, xq ahora la vista tambien mostrara unos mesajes con los errores econtrados
+            errors,//aqui mandamos los errores
             title,
-            description
-        });
+            description});
     }
     else{
-        res.send('ok');
+        res.send('ok');//response para ver algo y no se quede procesando
     }
-    console.log(req.body);
+    console.log(req.body);//req=request body es una propiead de la misma funcion para que reciba los datos, cuadno hagamos click en enviar asdicion
     
-});
+    });
+    
+
 module.exports = router;
 
 
