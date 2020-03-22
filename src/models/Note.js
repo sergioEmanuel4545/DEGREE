021c34dfffc0b7cc473400aff56bdb4f31b6d1ba
toshiba jhos <ser610.goy@tutanota.com>
2020-03-22 14:43:29 -0400

    avance toshiba 22-03-20

--- src/models/Note.js
+++ src/models/Note.js
@@ -1,13 +1,16 @@
 //utilizamos mongoose ya que sirve para CREAR SCHEMAS DE DATOS
 const mongoose = require('mongoose');
-const { Schema }= mongoose; // de mongoose solo nos importa el esquema y lo guardamos en una constante
+const { Schema }= mongoose; 
+// de mongoose solo nos importa el esquema y lo guardamos en una constante
 
 
 //describimos que propiedades van atener
 const NoteSchema = new Schema({
-    title: {type: String, required: true},//TRUE no puede estar vacio
+    title: {type: String, required: true},
+    //TRUE no puede estar vacio
     description: { type: String, required: true},
-    date: {type: Date, default: Date.now}//propiedad por defecto, si no le pasamos ningun dato pondra un valor por defecto 
+    date: {type: Date, default: Date.now}
+    //propiedad por defecto, si no le pasamos ningun dato pondra un valor por defecto 
 })
 
 //hay que decir a mongo db como crear el modelo
--- src/routes/notes.js
+++ src/routes/notes.js
@@ -9,7 +9,8 @@ const Note = require('../models/Note');
 
 //crear una ruta que le permita al usu ver un formulario
 router.get('/notes/add', (req,res) =>{
-    res.render('notes/new-note') //tenemos que crear un formulario(vista) en notes
+    res.render('notes/new-note')
+     //tenemos que crear un formulario(vista) en notes
 });
 
 
@@ -19,36 +20,52 @@ router.get('/notes/add', (req,res) =>{
 router.post('/notes/new-notes', async (req, res) => {
     //observamos que es un objeto deJS xq tiene llaves y adentro tiene propiedades, cada uno de los datos que envia el usuario es una propiedad
     //DESCTRUCTURING, poder sacar cada propiedad por separado en una constante o variable a partir de un objeto
-    const {title, description}= req.body;//quiero destructurar el title y el descrption, puden estar almacenados dentro una variable o constante, en este caso sera constante 
+    const {title, description}= req.body;
+    //quiero destructurar el title y el descrption, puden estar almacenados dentro una variable o constante, en este caso sera constante 
     //PROCESO DE VALIDACION
-    const errors = [];//creamos la constante errors como un arreglo
-    if(!title){//si en lo que me envia el usuario esta vacio
-        errors.push({text: 'Please write a title'});//insertamos en la const errors, un objeto que tendra como propiedad text: que tendra como valor:..
+    const errors = [];
+    //creamos la constante errors como un arreglo
+    if(!title){
+        //si en lo que me envia el usuario esta vacio
+        errors.push({text: 'Please write a title'});
+        //insertamos en la const errors, un objeto que tendra como propiedad text: que tendra como valor:..
     }
-    if(!description){//si usuario no envia description o si este se encuentra vacio
+    if(!description){
+        //si usuario no envia description o si este se encuentra vacio
         errors.push({text: 'please write a description' });
     }
-    if(errors.length > 0){//si el arreglo errors contine errores
-        res.render('notes/new-note', {//renderizamos nuevamente la vista de la insercion de los formularios, xq ahora la vista tambien mostrara unos mesajes con los errores econtrados
-            errors,//aqui mandamos los errores
+    if(errors.length > 0){
+        //si el arreglo errors contine errores
+        res.render('notes/new-note', {
+            //renderizamos nuevamente la vista de la insercion de los formularios, xq ahora la vista tambien mostrara unos mesajes con los errores econtrados
+            errors,
+            //aqui mandamos los errores
             title,
             description});
     }
     else{
         //res.send('ok');//response para ver algo y no se quede procesando y mandamos un ok como simulando que se inserto algun dato en la base de datos
-        const newNote =  new Note({ title, description });//aqui instanciamos la clase y le pasamos los datos, se crea nuevos datos para gurardar en mongo db
-        await newNote.save(); //para REALMENTE guardar en DB tenemos qy ejeutar un metodo llamdo save
+        const newNote =  new Note({ title, description });
+        //aqui instanciamos la clase y le pasamos los datos, se crea nuevos datos para gurardar en mongo db
+        await newNote.save(); 
+        //para REALMENTE guardar en DB tenemos qy ejeutar un metodo llamdo save
         //guardar en DB es un proceso asincrono, puesto que no sabemos cuento tiempo puede tardar hasta que termine el proceso de guardado, por lo que tenemos que declararlo en NODE.JS como AWAIT y en el metodo colocar "async" para que habilite la opcion de poner "await"
         //await= todo lo que esta debajo de await, se indica que espere para que termine la line adel await  
-        res.redirect('/notes');//despues que acabae el proceso de await voy a redireccionar a una vista, dandole una ruta
+        res.redirect('/notes');
+        //despues que acabae el proceso de await voy a redireccionar a una vista, dandole una ruta
     }    //req=request body es una propiead de la misma funcion para que reciba los datos, cuadno hagamos click en enviar asdicion
     });
- 
-    router.get('/notes', async (req,res) =>{ //aqui se crea la vista y el procedimiento a seguir
-    const notes = await Note.find()//jalamos los datos de la DB, dentro de find no le especificamos nada xq queremos que nos dvuelva todo, como no sabemos(igual que el guardado cuento pude tardar, entonces lo declaramos como asyncrono)
+ //la logica:cuando visites es ta ruta, que consulte a la base de datos y le pasamos la vista con las notas de la base de datos 
+    router.get('/notes', async (req,res) =>{ 
+        //aqui se crea la vista y el procedimiento a seguir
+    const notes = await Note.find().sort({date: 'desc'})
+    //jalamos los datos de la DB, dentro de find no le especificamos nada xq queremos que nos dvuelva todo, como no sabemos(igual que el guardado cuento pude tardar, entonces lo declaramos como asyncrono)
     //y lo almacenamos en una constante
-    res.render('notes/all-notes', {notes});
+    //ponemos .sort() para que organize los datos que encuentra en la base de datos, por la fecha de creacion de manera descendente
+    res.render('notes/all-notes', { notes });
 });
+
+router.get('notes/edit')
 module.exports = router;
 
 
